1b22242d48e88d195452d6b78ff6075c12eedeec
Pranay ponakanti <pranayponakani@gmail.com>
2025-08-09 08:14:16 -0700

    added loops program

---
+++ basicproblems.cpp/loops/loop.cpp
@@ -0,0 +1,16 @@
+// this programme is to print 1 to 10 numbers using while loop
+#include<iostream>
+using namespace std;
+int main(){
+    int n =0;
+    while(n<= 10){
+        cout<<n <<endl;
+        n++;
+    }
+    // using for loop
+    for (int i=0;i<=10;i++){
+        cout<<i<<endl;
+        
+    }
+    return 0;
+}
